9fd53fbdb3955c1f3c9a202a3b50c4e3c302ccae
Ezekiel <iamwebwiz@gmail.com>
2020-08-18 01:43:06 +0100

    Complete setting up React app with TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Fragment } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1 className="text-3xl text-indigo-600">Hello Tailwind!</h1>
+    </Fragment>
   );
 }
-
-export default App;
