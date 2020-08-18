9bc36136b14f6a73e4b151d979dc4f43ff02ed48
Ezekiel <iamwebwiz@gmail.com>
2020-08-18 02:05:39 +0100

    Add ProfileCard.jsx

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export const ProfileCard = () => {
+  return (
+    <Fragment>
+      <div className="text-indigo-600">What the fuck?</div>
+    </Fragment>
+  );
+};
