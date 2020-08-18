3a17b3ca2127acd3ccce95df0db9ec9cab309bb3
Ezekiel <iamwebwiz@gmail.com>
2020-08-19 00:52:00 +0100

    update profilecard component

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -8,7 +8,13 @@ export const ProfileCard = () => {
         className="bg-white rounded-lg shadow-lg grid justify-center p-10"
         style={styles.card}
       >
-        What the fuck?
+        <img
+          src="https://pbs.twimg.com/profile_images/1295121021503188995/EtTIzjFQ_400x400.jpg"
+          alt="iamwebwiz"
+          className="rounded-full w-48 h-48"
+        />
+
+        <h2 className="text-2xl text-gray-700 uppercase">Ezekiel Oladejo</h2>
       </div>
     </Fragment>
   );
