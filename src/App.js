cb87ca56d4721adb3687995305d67b47dd96eddf
Ezekiel <iamwebwiz@gmail.com>
2020-08-21 00:28:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import ProfileCard from "./components/ProfileCard";
+import usersList from "./users.json";
 
 export default function App() {
+  let [users] = useState(usersList);
+
   return (
     <Fragment>
-      <div className="w-screen h-screen bg-gray-200 grid justify-center items-center">
-        <ProfileCard />
+      <div className="w-screen bg-gray-200 px-16 py-20 grid md:grid-cols-3">
+        {users &&
+          users.map((user, index) => (
+            <ProfileCard
+              name={user.name}
+              role={user.role}
+              avatar={user.avatar}
+              key={index}
+            />
+          ))}
       </div>
     </Fragment>
   );
