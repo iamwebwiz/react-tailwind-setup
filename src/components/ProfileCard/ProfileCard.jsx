26211bbf0fae739df4f1c04cd6b2ac38b62d2b7b
Ezekiel <iamwebwiz@gmail.com>
2020-08-20 23:54:35 +0100

    Update ProfileCard component

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -8,13 +8,20 @@ export const ProfileCard = () => {
         className="bg-white rounded-lg shadow-lg grid justify-center p-10"
         style={styles.card}
       >
-        <img
-          src="https://pbs.twimg.com/profile_images/1295121021503188995/EtTIzjFQ_400x400.jpg"
-          alt="iamwebwiz"
-          className="rounded-full w-48 h-48"
-        />
+        <div className="flex justify-center">
+          <img
+            src="https://pbs.twimg.com/profile_images/1295121021503188995/EtTIzjFQ_400x400.jpg"
+            alt="iamwebwiz"
+            className="rounded-full w-48 h-48"
+          />
+        </div>
 
-        <h2 className="text-2xl text-gray-700 uppercase">Ezekiel Oladejo</h2>
+        <h2 className="text-2xl text-gray-700 uppercase text-center">
+          Ezekiel Oladejo
+        </h2>
+        <h3 className="text-lg text-blue-400 uppercase text-center text-sm">
+          Senior Software Engineer
+        </h3>
       </div>
     </Fragment>
   );
